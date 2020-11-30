9c2a4ab4cd75218e93b58fd50c9af73503cccaa2
Samir Karimov <samir@pragmatech.az>
2020-12-01 00:32:27 +0400

    Database location changed

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__,template_folder='../templates',static_folder='../static')
 db=SQLAlchemy(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:/Users/User/Desktop/updatedproject/industry.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///./industry.db"
 db = SQLAlchemy(app)
 from app import models
 from app import views
